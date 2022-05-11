9129a1b64c1ead4f6b4214f2a9fa5698e00efc62
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-05-11 23:57:07 +0300

    асинхронна функція

--- src/js/markupCard.js
+++ src/js/markupCard.js
@@ -32,8 +32,8 @@ const galleryRef = document.querySelector('[data-gallery]')
 
 const apiKey = '9247604179ba4f8029cfb34035e24946';
 
-function fetchTrending() {
-    return fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=${apiKey}&language=uk`)
+async function fetchTrending() {
+    return await fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=${apiKey}&language=uk`)
         .then(response => {
             if (!response.ok) {
                 throw Error(response.status);
